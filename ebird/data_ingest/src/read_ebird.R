ec3395bde966aa1996784021c8e10c50131f4fe3
mschulist2@gmail.com <mschulist2@gmail.com>
2022-01-15 17:50:34 -0800

    Started spatial mapping

--- ebird/data_ingest/src/read_ebird.R
+++ ebird/data_ingest/src/read_ebird.R
@@ -11,3 +11,16 @@ library(data.table)
 library(auk)
 
 source(here("gen_funs.R"))
+
+# Syncing eBird data with google drive -------------
+drive_sync(here("ebird/data_ingest/input/"), "https://drive.google.com/drive/u/0/folders/1Ts-wV_VCq2eK5TCLgILB2_lm108OcrQM")
+
+# Setting Filter Values ------------------
+species <- c("")
+
+# Setting up eBird directory and filtering ----------------
+ebd_filters <- auk_ebd(here("ebird/data_ingest/input/")) %>% 
+  auk_species(species) %>% 
+  auk_complete()
+
+auk_filter(ebd_filters, file = here("ebird/data_ingest/output/ebd_output.txt"))
---
+++ spatial/data_ingest/src/spatial.R
@@ -0,0 +1,20 @@
+## File for creating the map and combining the eBird and fish data
+## Mark Schulist - January 2022
+
+# Loading Libraries --------------
+library(tidyverse)
+library(here)
+library(googledrive)
+library(data.table)
+library(ggmap)
+
+source(here("gen_funs.R"))
+# source(here("ebird/data_ingest/src/read_ebird.R"))
+
+# Making the map and inputting area of map
+river_map <- get_stamenmap(
+  bbox = c(left = -123.8, bottom = 37.1, right = -120.8, top = 41.5), 
+  maptype = "terrain", 
+  zoom = 11
+) %>% 
+  ggmap()
