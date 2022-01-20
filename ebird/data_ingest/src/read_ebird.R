71b151d5152a72ec16b560140747b29640f431c5
mschulist <mschulist2@gmail.com>
2022-01-20 15:12:56 -0800

    added unzipping

--- ebird/data_ingest/src/read_ebird.R
+++ ebird/data_ingest/src/read_ebird.R
@@ -9,3 +9,23 @@ library(data.table)
 library(auk)
 
 source(here("gen_funs.R"))
+
+# Downloading and unzipping eBird Data if not already downloaded --------------
+
+# Creating input folder
+if (dir.exists(here("ebird/data_ingest/input/")) == F) {
+  dir.create(here("ebird/data_ingest/input/"))
+}
+
+# Checking if zip file has been downloaded and downloading if not already downloaded
+if (file.exists(here("ebird/data_ingest/input/ebd_US-CA_relDec-2021.zip")) == F) {
+  drive_download("https://drive.google.com/file/d/1YxCmF4VdNxxsEzZbziJg6YonA0TC7bQe/view?usp=sharing", here("ebird/data_ingest/input/ebd_US-CA_relDec-2021.tar.gz"))
+}
+
+# Unzipping file if not already unzipped
+if (dir.exists(here("ebird/data_ingest/input/ebd_US-CA_relDec-2021/")) == F) {
+  unzip(here("ebird/data_ingest/input/ebd_US-CA_relDec-2021.tar.gz"), exdir = here("ebird/data_ingest/input/"))
+}
+
+
+
