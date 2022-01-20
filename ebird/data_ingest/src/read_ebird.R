34486e0119773bfecd6836d9647812c9844a0bfa
mschulist2@gmail.com <mschulist2@gmail.com>
2022-01-20 07:49:43 -0800

    started read ebird filtering

---
+++ ebird/data_ingest/src/read_ebird.R
@@ -0,0 +1,11 @@
+## read_ebird.R
+## Mark Schulist - January 2022
+
+# Loading Libraries --------------------
+library(tidyverse)
+library(here)
+library(googledrive)
+library(data.table)
+library(auk)
+
+source(here("gen_funs.R"))
