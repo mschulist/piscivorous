55a9e0ccb95bd528c7c79f77d09b6da6c58e3237
Joseph Zeno <joseph.h.zeno@gmail.com>
2022-01-22 18:01:38 -0800

    styled script

--- ebird/summarize/src/summarize_ebird.R
+++ ebird/summarize/src/summarize_ebird.R
@@ -29,8 +29,8 @@ link_create(here("ebird/data_ingest/output/ebd_filtered.txt"), here("ebird/summa
 ebd_data <- fread(here("ebird/summarize/input/ebd_filtered.txt"))
 
 # Filtering eBird data ---------------
-dcco_ebd_data <- ebd_data %>% 
-  filter(`COMMON NAME` == "Double-crested Cormorant") %>% 
+dcco_ebd_data <- ebd_data %>%
+  filter(`COMMON NAME` == "Double-crested Cormorant") %>%
   filter(`OBSERVATION COUNT` != "X")
 
 # Getting rid of "X"
@@ -43,23 +43,23 @@ if (dir.exists(here("ebird/summarize/input/EDSM_subregions")) == F) {
 }
 drive_sync(here("ebird/summarize/input/EDSM_subregions/"), "https://drive.google.com/drive/u/0/folders/1N-Xgf39IlL4ZW61A2dDq687U79dgWQ7x")
 
-delta_map <- st_read(here("ebird/summarize/input/EDSM_subregions/EDSM_subregions_120418_Phase1.shp")) %>% 
+delta_map <- st_read(here("ebird/summarize/input/EDSM_subregions/EDSM_subregions_120418_Phase1.shp")) %>%
   st_transform(crs = 4326) # Changing the coord system to the ebird (correct) coord system
 
 
 # Making eBird data into sf object
-dcco_sf <- dcco_ebd_data %>% 
+dcco_sf <- dcco_ebd_data %>%
   st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
 # Joining eBird and polygon data
-dcco_ebd_data_join <- st_join(dcco_sf, delta_map, left = T) %>% 
+dcco_ebd_data_join <- st_join(dcco_sf, delta_map, left = T) %>%
   filter(!is.na(SQM))
 
 # Summarizing the dcco data by month (for now)
-dcco_summary <- dcco_ebd_data_join %>% 
-  rename(date = `LAST EDITED DATE`) %>% #renaming date to make it pretty
+dcco_summary <- dcco_ebd_data_join %>%
+  rename(date = `LAST EDITED DATE`) %>% # renaming date to make it pretty
   mutate(
-    month_yr = format_ISO8601(date, precision = "ym") #only keeping the month and year
-    ) %>% 
-  group_by(SQM, SubRegion, month_yr) %>% 
-  filter(n() >= 3) %>% #getting rid of groups that do not have at least 3 data points
+    month_yr = format_ISO8601(date, precision = "ym") # only keeping the month and year
+  ) %>%
+  group_by(SQM, SubRegion, month_yr) %>%
+  filter(n() >= 3) %>% # getting rid of groups that do not have at least 3 data points
   summarize(count = mean(`OBSERVATION COUNT`), st_dev = sd(`OBSERVATION COUNT`))
